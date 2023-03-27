eff7544553567d20c10f4d4381606b7ab132432f
Jean <jeancln21@gmail.com>
2023-03-27 19:36:35 +0200

    add work and filterbtn with js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,9 +2,9 @@
 /* Déclarations des variables utilisable sur tout le fichier */
 
 // constante pointe sur la div filtre et la gallery dans le DOM
-const allFilters = document.querySelector(".all-filters")
+const allFilters = document.querySelector(".all-filters");
 
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
 
 
 /* Fonctions pour la partie filtres et gallery */
@@ -12,7 +12,7 @@ const gallery = document.querySelector(".gallery")
 // Fonction création travaux et insère dans la gallery  ( potentiellement séparer en plus petites )
 
 
-function addWork() {
+function addWork(figure) {
 
   // creation et stockage des nouveaux elements 
   let newFigure = document.createElement("figure")
@@ -20,8 +20,8 @@ function addWork() {
   let newFigcaption = document.createElement("figcaption")
   
   // création et stockage du contenu des nouveaux éléments
-  // newImage.src = figure.imageUrl
-  // newFigcaption = document.createTextNode(figure.title);
+  newImage.src = figure.imageUrl
+  newFigcaption = document.createTextNode(figure.title);
 
   // création de l'arborescance figure
   newFigure.appendChild(newImage)
@@ -33,43 +33,29 @@ function addWork() {
 
 
 // Fonction création button filtres et insère dans la div all-filters  ( potentiellement séparer en plus petites )
-function addFilter() {
+function addFilter(filter) {
     // Création des noeuds
     const filterButton = document.createElement("button")
+    
+    // injection de la class not-actived aux buttons
+    filterButton.classList.add("not-actived")
 
     // texte sous les images
-    filterButton.innerText = ""
+    filterButton.innerText = filter.name
                 
     // insère travaux dans la div filter
     allFilters.appendChild(filterButton)   
     console.log(filterButton);                       
 }
-
-
-/* Création class pour faciliter le traitement après récupération des API via le fetch */
-
-// Création d'une class Figure 
-class Figure {
-  constructor(jsonFigure){
-    jsonFigure && Object.assign(this, jsonFigure)
-  }
-}
-
-// Création d'une class Filter   
-  class Filter {
-    constructor(jsonFilter){
-      jsonFilter && Object.assign(this, jsonFilter)
-    }
-  }
-  
+ 
 
 /* récupération des API pour l'ajout et le filtrage dynamique des travaux via fetch */
 
 // stockage de la récupération des travaux dans une fonction asynch
 async function getWorks () {
 
-    // requête API travaux
-    await fetch('http://localhost:5678/api/works')
+  // requête API travaux
+  await fetch('http://localhost:5678/api/works')
 
     // récupération des données dans "response"
     .then((response) => {
@@ -89,9 +75,8 @@ async function getWorks () {
 
       // Pour chacunes des figures des données du tableau récupérées, 
       jsonFigureList.forEach(jsonFigure => {
-        // stockage d'une nouvelle instance de la class Figure dans une variable figure
-        let figure = new Figure(jsonFigure)
-        addWork(figure.image)
+        // appel de la fonction addWork 
+        addWork(jsonFigure)
       });
     })
     // affichage du potentiel problème avec le fetch
@@ -102,4 +87,37 @@ async function getWorks () {
 
 getWorks()
 
+// stockage de la récupération des catégories dans une fonction asynch
+async function getFilters () {
+  
+  // requête API catégories
+  await fetch('http://localhost:5678/api/categories')
 
+  // récupération des données dans "response"
+  .then((response) => {
+
+    // requête en cas de réussite
+    if (response.ok) {
+    // récupération des données au format json
+      return response.json()
+    // sinon affiche un problème de réseau dans la console
+    } else {      
+      console.log('mauvaise réponse du réseau');
+    }
+  })
+  // nouvelle requête et stock les données de response.json dans jsonFilterList
+  .then ( jsonFilterList => {
+
+    // Pour chacunes des figures des données du tableau récupérées, 
+    jsonFilterList.forEach(jsonFilter => {
+      // appel de la fonction addWork
+      addFilter(jsonFilter)
+    });
+  })
+  // affichage du potentiel problème avec le fetch
+  .catch((error) => {
+    console.log("il y a eu un problème avec l'opération fetch : " + error.message);
+  })
+}
+
+getFilters();
